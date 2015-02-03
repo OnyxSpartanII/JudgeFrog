e6f4db0a2fb0e7452e7d8ecb1033078f4fc99d9f
ahabthearab7 <cgduncan7@gmail.com>
2015-02-02 19:49:08 -0600

    Fixed sliderMod.js

--- jdgfrog/app/webroot/js/sliderMod.js
+++ jdgfrog/app/webroot/js/sliderMod.js
@@ -1,330 +1,323 @@
-$(function () {
+$( function () {
 
-    //Case Sliders
-      $("#numbDefSlide").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-    //Denfendant Sliders
-      $("#yearBirthDefendant").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 1900,
-            max: 2020,
-            from: 1970,
-            to: 2002,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });  
-    //Judge Sliders
-      $("#yearAppointJudge").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 1900,
-            max: 2020,
-            from: 1970,
-            to: 2002,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-    //Victim Sliders
-      $("#totalSlide").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-        $("#minorSlide").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-        $("#foreignerSlide").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-        $("#femaleSlide").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-    //Arrest Details Sliders
-      $("#dateArrestAD").ionRangeSlider({ /* Do we want just year or date */
+	//Case Sliders
+	$( "#numbDefSlide" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 100,
+		from: 0,
+		to: 100,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	//Denfendant Sliders
+	$( "#yearBirthDefendant" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 1930,
+		max: 2014,
+		from: 1930,
+		to: 2014,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	//Judge Sliders
+	$( "#yearAppointJudge" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 1960,
+		max: 2020,
+		from: 1960,
+		to: 2020,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	//Victim Sliders
+	$( "#totalSlide" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 100,
+		from: 0,
+		to: 100,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#minorSlide" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 100,
+		from: 0,
+		to: 100,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#foreignerSlide" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 100,
+		from: 0,
+		to: 100,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#femaleSlide" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 100,
+		from: 0,
+		to: 100,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	//Arrest Details Sliders
+	$( "#dateArrestAD" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 2000,
+		max: 2020,
+		from: 2000,
+		to: 2020,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#bailAmountArrest" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 1000,
+		max: 100000,
+		from: 1000,
+		to: 100000,
+		type: 'integer',
+		step: 1,
+		prefix: "$",
+		max_postfix: '+',
+		grid: true
+	} );
+	//Charge Details Sliders
+	$( "#chargeDate" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 2000,
+		max: 2020,
+		from: 2000,
+		to: 2020,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#totalCharges" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 20,
+		from: 0,
+		to: 20,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#countsCharge" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#countsNolleProssed" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#pleaDismiss" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#pleaGuilty" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#trialGuilty" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#trialNotGuilty" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#sentenceCharge" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 300,
+		from: 0,
+		to: 300,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#probationCharge" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 300,
+		from: 0,
+		to: 300,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
 
-             min: +moment().subtract(1, "years").format("X"),
-             max: +moment().format("X"),
-              from: +moment().subtract(6, "months").format("X"),
-              prettify: function (num) {
-        return moment(num, "X").format("LL");
-      } /*
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true */
-        }); 
-      $("#bailAmountArrest").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 1000000,
-            from: 250000,
-            to: 750000,
-            type: 'double',
-            step: 1,
-            prefix: "$",
-            grid: true
-        }); 
-    //Charge Details Sliders
-      $("#chargeDate").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#totalCharges").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#countsCharge").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#countsNolleProssed").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#pleaDismiss").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#pleaGuilty").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#trialGuilty").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#trialNotGuilty").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#finesCharge").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "$",
-            grid: true
-        }); 
-      $("#sentenceCharge").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-      $("#probationCharge").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        });
-
-    //Sentencing Details Sliders
-      $("#totalNumFelonySentence").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-            }); 
-      $("#dateTermSentenced").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-            }); 
-      $("#totalMonthsSentenced").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-            }); 
-      $("#amountRestitutionSent").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-            }); 
-      $("#monthsSuperReleaseSentence").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-            }); 
-      $("#monthsProbationSentence").ionRangeSlider({
-            hide_min_max: true,
-            keyboard: true,
-            min: 0,
-            max: 5000,
-            from: 1000,
-            to: 4000,
-            type: 'double',
-            step: 1,
-            prefix: "",
-            grid: true
-        }); 
-});
+	//Sentencing Details Sliders
+	$( "#totalNumFelonySentence" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10,
+		from: 0,
+		to: 10,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#dateTermSentenced" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 2000,
+		max: 2020,
+		from: 2000,
+		to: 2020,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#totalMonthsSentenced" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 300,
+		from: 0,
+		to: 300,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#amountRestitutionSent" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 10000000,
+		from: 0,
+		to: 10000000,
+		type: 'integer',
+		step: 1,
+		prefix: "$",
+		max_postfix: '+',
+		grid: true
+	} );
+	$( "#monthsProbationSentence" ).ionRangeSlider( {
+		hide_min_max: true,
+		keyboard: true,
+		min: 0,
+		max: 50,
+		from: 0,
+		to: 50,
+		type: 'integer',
+		step: 1,
+		prefix: "",
+		max_postfix: '+',
+		grid: true
+	} );
+} );
